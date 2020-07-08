7e2b89efb4e133c86625ec1c29c783018a73a250
misti <yallalraj1307@gmail.com>
2020-07-08 20:46:59 +0530

    [Rajeshwari] ADDED: function for resetting the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,4 +1,25 @@
 #!/bin/bash -x
+tput clear
+echo "Welcome to the TIC TAC TOE GAME SIMULATOR"
 
-echo "Welcome to Tic Tac Toe Game ...!"
+function createBoardReset()
+{
+        board=( " " 1 2 3 4 5 6 7 8 9 )
+        echo "Game is Starting now........."
+}
 
+function createBoard()
+{
+        echo "${board[1]} | ${board[2]} | ${board[3]}"
+        echo "-----------"
+        echo "${board[4]} | ${board[5]} | ${board[6]}"
+        echo "-----------"
+        echo "${board[7]} | ${board[8]} | ${board[9]}"
+
+}
+
+createBoard
+createBoardReset
+echo "Setting up the board and creating"
+sleep 1
+createBoard
