78a35ee68f858adbbe842b8ab37950658f096cd2
misti <yallalraj1307@gmail.com>
2020-07-08 23:34:41 +0530

    [Rajeshwari] ADDED: function to determine after every move the winner, tie or changed turn.

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -119,7 +119,6 @@ function computerPlay()
         while(($computerCheck==0))
         do
                 repeatPlayerComputer
-                echo "COMPUTER is entering new value where you want to put out of ${board[@]}"
                 place=$((RANDOM%9 + 1))
                 repeatPlayerComputer
                 continue
@@ -135,17 +134,75 @@ function computerPlay()
         echo "---------------------------------------------------------------------------------"
 }
 
+winPlayer=0
+winComputer=0
+function testWin()
+{
+        if(($toss==0 || $toss==1))
+        then
+
+                if [[ ${board[$1]} == 0 ]]
+                then
+                        if [[ ${board[$2]} == 0 ]]
+                        then
+                                if [[ ${board[$3]} == 0 ]]
+                                then
+                                        winPlayer=1
+                                        winComputer=1
+                                fi
+                        fi
+                elif [[ ${board[$1]} == x ]]
+                then
+                        if [[ ${board[$3]} == x ]]
+                        then
+                                if [[ ${board[$2]} == x ]]
+                                then
+                                        winComputer=1
+                                        winPlayer=1
+                                fi
+                        fi
+                fi
+        fi
+}
+
+function checkWin()
+{
+        testWin 1 2 3 testWin 2 3 1 testWin 3 1 2 testWin 3 2 1 testWin 2 1 3 testWin 1 3 2
+        testWin 4 5 6 testWin 5 6 4 testWin 6 4 5 testWin 6 5 4 testWin 5 4 6 testWin 4 6 5
+        testWin 7 8 9 testWin 8 9 7 testWin 9 7 8 testWin 9 8 7 testWin 8 7 9 testWin 7 9 8
+        testWin 1 4 7 testWin 4 7 1 testWin 7 1 4 testWin 7 4 1 testWin 4 1 7 testWin 1 7 4
+        testWin 2 5 8 testWin 5 8 2 testWin 8 2 5 testWin 8 5 2 testWin 5 2 8 testWin 2 8 5
+        testWin 3 6 9 testWin 6 9 3 testWin 9 3 6 testWin 9 6 3 testWin 6 3 9 testWin 3 9 6
+        testWin 1 5 9 testWin 5 9 1 testWin 9 1 5 testWin 9 5 1 testWin 5 1 9 testWin 1 9 5
+        testWin 3 5 7 testWin 5 7 3 testWin 7 3 5 testWin 7 5 3 testWin 5 3 7 testWin 3 7 5
+
+}
+
 if(($toss==0))
 then
         while(($leftPlayer != 1))
         do
                 playerPlay
+                checkWin
+                if(($winPlayer==1))
+                then
+                        echo "--------------player is winner-------------------"
+                        exit
+                fi
                 if(($leftComputer!=2))
                 then
                         computerPlay
-                sleep 1
+                        sleep 1
+                        checkWin
+                        if(($winComputer==1))
+                        then
+                                echo "--------------------computer is winner-------------------"
+                                exit
+                        fi
+
                 fi
         done
+        echo "game is tie !! no one is winner"
         echo "game is exiting"
         exit
 else
@@ -153,11 +210,25 @@ else
         do
                 computerPlay
                 sleep 1
+                        checkWin
+                        if(($winComputer==1))
+                        then
+                                echo "---------------------------computer is winner----------------------------"
+                                exit
+                        fi
                 if(($leftPlayer!=2))
                 then
                         playerPlay
+                        checkWin
+                        if(($winPlayer==1))
+                        then
+                                echo "-------------------------------player is winner------------------------------"
+                                exit
+                        fi
                 fi
         done
+        echo "game is tie !! no one is winner"
         echo "game is exiting"
         exit
 fi
+
